cc9df86fca14bdae5621ab3d406fd8662d54a605
sofoniasgd <zephaniahsofa@gmail.com>
2024-10-02 04:46:38 +0300

    updated task 2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,10 +1,34 @@
 function countStudents(path) {
   const fs = require('fs');
-
+  let studentNumber = 0;
+  const departments = {};
   fs.readFile(path, 'utf8', (err, data) => {
     if (err) {
-      throw new Error("Cannot load the database");
-    } 
+      throw new Error('Cannot load the database');
+    }
+    const lines = data.split(/\r?\n/);
+    for (let i = 1; i < lines.length; i += 1) {
+      if (lines[i] === '') {
+        continue;
+      }
+      // increment number of students
+      studentNumber += 1;
+      // get department and student name
+      const words = lines[i].split(',');
+      const dep = words[words.length - 1];
+      // create a list if it doesnt exist and append student name
+      if (departments[dep] === 'undefined' || !(departments[dep] instanceof Array)) {
+        departments[dep] = [];
+      }
+      departments[dep].push(words[0]);
+    }
+    console.log('Number of students: %d', studentNumber);
+    if (departments) {
+      for (const dep in departments) {
+        console.log('Number of students in %s: %d', dep, departments[dep].length);
+        console.log(departments[dep]);
+      }
+    }
   });
 }
 

bee80575c44731f3a7f9103732f86e62055222b5
sofoniasgd <zephaniahsofa@gmail.com>
2024-10-03 12:21:59 +0300

    updated task 2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -9,25 +9,24 @@ function countStudents(path) {
     }
     const lines = data.split(/\r?\n/);
     for (let i = 1; i < lines.length; i += 1) {
-      if (lines[i] === '') {
-        continue;
+      if (lines[i] !== '') {
+        // increment number of students
+        studentNumber += 1;
+        // get department and student name
+        const words = lines[i].split(',');
+        const dep = words[words.length - 1];
+        // create a list if it doesnt exist and append student name
+        if (departments[dep] === 'undefined' || !(departments[dep] instanceof Array)) {
+          departments[dep] = [];
+        }
+        departments[dep].push(words[0]);
       }
-      // increment number of students
-      studentNumber += 1;
-      // get department and student name
-      const words = lines[i].split(',');
-      const dep = words[words.length - 1];
-      // create a list if it doesnt exist and append student name
-      if (departments[dep] === 'undefined' || !(departments[dep] instanceof Array)) {
-        departments[dep] = [];
-      }
-      departments[dep].push(words[0]);
     }
     console.log('Number of students: %d', studentNumber);
     if (departments) {
-      for (const dep in departments) {
-        const names = departments[dep].join(', ');
-        console.log('Number of students in %s: %d. List: %s', dep, departments[dep].length, names);
+      for (const [key, value] of Object.entries(departments)) {
+        const names = value.join(', ');
+        console.log(`Number of students in ${key}: ${value.length}. List: ${names}`);
       }
     }
   });
